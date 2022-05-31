6cd648220c407927f477352d11f325d782ac624e
Navdeep <omkumar@Oms-MacBook-Air.local>
2022-06-01 00:50:35 +0530

    New changes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const authApp = require("./authentication");
 const userApp = require("./user");
 const cors = require("cors");
 const streamApp = require("./stream");
+const path = require("path");
 const scheduleApp = require("./schedule");
 mongoose
   .connect(
@@ -13,7 +14,7 @@ mongoose
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
+      useCreateIqndex: true,
     }
   )
   .then(() => {
@@ -29,6 +30,13 @@ app.use("/api/auth", authApp);
 app.use("/api/user", userApp);
 app.use("/api/stream", streamApp);
 app.use("/api/schedule", scheduleApp);
+app.use(express.static(path.join(__dirname, "./web-build")));
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'frontend-build', 'index.html'));
+// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./web-build", "index.html"));
+});
 
 app.listen(9999, () => {
   console.log("Server running");
